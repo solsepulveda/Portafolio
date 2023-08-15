45ad1f446b79c152f051eb47dc19d44bc2ed4b17
solsepulveda <solangesepulved@gmail.com>
2023-08-15 19:24:33 -0400

    carrousel

--- src/main.js
+++ src/main.js
@@ -1,23 +1,33 @@
 function showContainer(containerNumber) {
-    // Ocultar todos los contenedores
-    const containers = document.querySelectorAll('.projects section[class^="contenedor-"]');
-    containers.forEach(container => container.classList.remove('show'));
-  
-    // Mostrar el contenedor seleccionado
-    const selectedContainer = document.querySelector(`.contenedor-${containerNumber}`);
-    selectedContainer.classList.add('show');
-  };
+  // Ocultar todos los contenedores
+  const containers = document.querySelectorAll('.projects section[class^="contenedor-"]');
+  containers.forEach(container => container.classList.remove('show'));
 
-  window.addEventListener("scroll",()=>{
-    var navBar= document.querySelector(".container");
-    navBar.classList.toggle("sticky",window.scrollY>0)
-  })
+  // Mostrar el contenedor seleccionado
+  const selectedContainer = document.querySelector(`.contenedor-${containerNumber}`);
+  selectedContainer.classList.add('show');
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
-    const navMenu = document.querySelector(".nav-main ul");
-  
-    mobileMenuBtn.addEventListener("click", function () {
-      navMenu.classList.toggle("active");
-    });
+  // Highlight the selected button and remove highlight from others
+  const options = document.querySelectorAll('.options li');
+  options.forEach((option, index) => {
+    if (index === containerNumber - 1) {
+      option.classList.add('active');
+    } else {
+      option.classList.remove('active');
+    }
   });
+}
+
+window.addEventListener("scroll", () => {
+  var navBar = document.querySelector(".container");
+  navBar.classList.toggle("sticky", window.scrollY > 0);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
+  const navMenu = document.querySelector(".nav-main ul");
+
+  mobileMenuBtn.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
+  });
+});
